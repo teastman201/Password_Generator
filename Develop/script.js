0fd74ad7dead43361a00f8defabdf8d50599a57d
Tristan Eastman <teastman85@gmail.com>
2020-08-02 06:44:34 -0400

    Added user prompt for password length. Removed alert to include letters.

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,19 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  userPrompt = window.prompt("How long would you like your password to be (enter number between 8 and 128)");
+
   userSelectInt = window.confirm("Would you like to include integers (0123456789)?");
-  userSelectChar = window.confirm("Would you like to include letters (abcdefghijklmnopqrstuvwxyz)?");
+  // userSelectChar = window.confirm("Would you like to include letters (abcdefghijklmnopqrstuvwxyz)?");
   userSelectSpec = window.confirm("Would you like to include special characters (~`!@#$%^&*().?-_+={}[]|\/:;'<>)?");
+
+  intArray = "0123456789";
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  if (userSelectInt) = true {
+
+  if (userSelectInt) {
 
   }
   

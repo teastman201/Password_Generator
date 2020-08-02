77ce5facb11fcefb96bf26a944a825337e978a31
Tristan Eastman <teastman85@gmail.com>
2020-08-02 11:14:44 -0400

    adding logic for password generation

--- Develop/script.js
+++ Develop/script.js
@@ -3,26 +3,68 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(length) {
 
-  userPrompt = window.prompt("How long would you like your password to be (enter number between 8 and 128)");
+  userPrompt = window.prompt("How many characters would you like your password to have (enter number between 8 and 128)");
+  userIncludeInt = window.confirm("Would you like to include integers (0123456789)?");  
+  userIncludeSpec = window.confirm("Would you like to include special characters (~!@#$%^&*.?-_+={}()[]|\/<>)?");
 
-  userSelectInt = window.confirm("Would you like to include integers (0123456789)?");
-  // userSelectChar = window.confirm("Would you like to include letters (abcdefghijklmnopqrstuvwxyz)?");
-  userSelectSpec = window.confirm("Would you like to include special characters (~`!@#$%^&*().?-_+={}[]|\/:;'<>)?");
+  var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var specChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*.?-_+={}()[]|\/<>';
+  var numbers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+  let numPass = '';
+  let specPass = '';
 
-  intArray = "0123456789";
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  if (userSelectInt) {
-
+  if (userIncludeInt && userIncludeSpec) {
+    for (let i = 0; i < userPrompt; i++) {
+      numPass += specChar.charAt(Math.floor(Math.random() * specChar.length));
+    }
+  } else {
+    for (let i = 0; i < userPrompt; i++) {
+      numPass += letters.charAt(Math.floor(Math.random() * letters.length));
   }
+}
+/* 
+  if (userIncludeSpec) {
+    for (let i = 0; i < userPrompt; i++) {
+      specPass += specChar.charAt(Math.floor(Math.random() * specChar.length));
+    }
+  } else {
+    for (let i = 0; i < userPrompt; i++) {
+      specPass += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    }
+  }
+*/
   
+    /*
+  if (userIncludeInt) {
+    for ( var i = 0; i < userPrompt; i++ ) {
+      value1 = numbers.charAt(Math.floor(Math.random() * userPrompt));
+   }
+  }
 
-  passwordText.value = password;
-  
+  if (userIncludeSpec) {
+    for ( var s = 0; s < specChar.length; s++ ) {
+      value2 = specChar.charAt(Math.floor(Math.random() * specChar.length));
+   }
+  }
+
+  if (userIncludeInt && userIncludeSpec) {
+    for ( var a = 0; a < allChar.length; a++ ) {
+      value3 = allChar.charAt(Math.floor(Math.random() * allChar.length));
+ }
+}
+*/ 
+
+console.log(numPass);
+console.log(specPass);
+
+  //var password = generatePassword();
+ // var passwordText = document.querySelector("#password");
+ 
+ // passwordText.value = password;
+
 }
 
 // Add event listener to generate button

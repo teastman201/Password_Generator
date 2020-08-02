49dff6594bebfec91bb0dadf8b62f3644bc26011
Tristan Eastman <teastman85@gmail.com>
2020-08-02 06:11:47 -0400

    Added user prompt for password criteria.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
+  userSelectInt = window.prompt("Would you like to include integers?");
+  userSelectChar = window.prompt("Would you like to include letters?");
+  userSelectSpec = window.prompt("Would you like to include special characters?");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button

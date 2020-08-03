2538088f2935ca84ced59f3c14078c26ae5f6a4f
Tristan Eastman <teastman85@gmail.com>
2020-08-03 05:52:51 -0400

    refactored code to be more concise.

--- Develop/script.js
+++ Develop/script.js
@@ -3,76 +3,46 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword(length) {
+function writePassword() {
 
   userPrompt = window.prompt("How many characters would you like your password to have (enter number between 8 and 128)");
+  userIncludeUpper = window.confirm("Would you like to include upper case letters (ABCDEFGHIJKLMNOPQRSTUVWXYZ)?");  
+  userIncludeLower = window.confirm("Would you like to include lower case letters (abcdefghijklmnopqrstuvwxyz)?");  
   userIncludeInt = window.confirm("Would you like to include integers (0123456789)?");  
   userIncludeSpec = window.confirm("Would you like to include special characters (~!@#$%^&*.?-_+={}()[]|\/<>)?");
 
-  var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  var specChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*.?-_+={}()[]|\/<>';
-  var numbers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var passwordText = document.querySelector("#password");
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  var numbers = '0123456789';
+  var specChar = '~!@#$%^&*.?-_+={}()[]|\/<>';
+  var possible = '';
   
-  let numPass = '';
-  let specPass = '';
+  let userPass = '';
 
-  if (userIncludeInt && userIncludeSpec) {
-    for (let i = 0; i < userPrompt; i++) {
-      numPass += specChar.charAt(Math.floor(Math.random() * specChar.length));
-    }
-  } else {
-    for (let i = 0; i < userPrompt; i++) {
-      numPass += letters.charAt(Math.floor(Math.random() * letters.length));
+  if (userIncludeUpper) {
+    possible += upperCase;
   }
-}
 
-  if (userIncludeSpec) {
-    for (let i = 0; i < userPrompt; i++) {
-      specPass += specChar.charAt(Math.floor(Math.random() * specChar.length));
-    }
-  } else {
-    for (let i = 0; i < userPrompt; i++) {
-      numPass += letters.charAt(Math.floor(Math.random() * letters.length));
-    }
+  if (userIncludeLower) {
+    possible += lowerCase;
   }
 
   if (userIncludeInt) {
-    for (let i = 0; i < userPrompt; i++) {
-      specPass += specChar.charAt(Math.floor(Math.random() * specChar.length));
-    }
-  } else {
-    for (let i = 0; i < userPrompt; i++) {
-      numPass += letters.charAt(Math.floor(Math.random() * letters.length));
-    }
+    possible += numbers;
   }
-  
-    /*
-  if (userIncludeInt) {
-    for ( var i = 0; i < userPrompt; i++ ) {
-      value1 = numbers.charAt(Math.floor(Math.random() * userPrompt));
-   }
-  }
-
   if (userIncludeSpec) {
-    for ( var s = 0; s < specChar.length; s++ ) {
-      value2 = specChar.charAt(Math.floor(Math.random() * specChar.length));
-   }
+    possible += specChar;
   }
 
-  if (userIncludeInt && userIncludeSpec) {
-    for ( var a = 0; a < allChar.length; a++ ) {
-      value3 = allChar.charAt(Math.floor(Math.random() * allChar.length));
- }
-}
-*/ 
+  for (let i = 0; i < userPrompt; i++) {
+    userPass += possible.charAt(Math.floor(Math.random() * possible.length));
+  }  
 
-console.log(numPass);
-console.log(specPass);
 
-  //var password = generatePassword();
- // var passwordText = document.querySelector("#password");
+//var password = generatePassword();
  
- // passwordText.value = password;
+passwordText.value = userPass;
 
 }
 

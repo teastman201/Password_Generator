eb1d7a7a19105d58da525b2ea854e25b537c75c9
Tristan Eastman <teastman85@gmail.com>
2020-08-03 20:24:39 -0400

    added if statement for alert window when user does not select a criteria.

--- script.js
+++ script.js
@@ -11,31 +11,32 @@ function writePassword() {
   var numbers = '0123456789';
   var specChar = '~!@#$%^&*?-_+={}()[]|\/<>';
   var possible = '';
-  
+
   let userPass = '';
 
-  let userPrompt = window.prompt("How many characters would you like your password to have (enter number between 8 and 128)");
-  
+  let userPrompt;
+
   do {
     userPrompt = window.prompt("How many characters would you like your password to have (enter number between 8 and 128)");
-  } 
-  while (userPrompt < 8 || userPrompt > 128);    
+  }
+  while (userPrompt < 8 || userPrompt > 128);
 
-  let userIncludeUpper = window.confirm("Would you like to include upper case letters (ABCDEFGHIJKLMNOPQRSTUVWXYZ)?");  
-  let userIncludeLower = window.confirm("Would you like to include lower case letters (abcdefghijklmnopqrstuvwxyz)?");  
-  let userIncludeInt = window.confirm("Would you like to include integers (0123456789)?");  
-  let userIncludeSpec = window.confirm("Would you like to include special characters (~!@#$%^&*.?-_+={}()[]|\/<>)?");
-     
+  let userIncludeUpper;
+  let userIncludeLower;
+  let userIncludeInt;
+  let userIncludeSpec;
+
   do {
-    window.alert("You must select at least one category.");
     userIncludeUpper = window.confirm("Would you like to include upper case letters (ABCDEFGHIJKLMNOPQRSTUVWXYZ)?");
     userIncludeLower = window.confirm("Would you like to include lower case letters (abcdefghijklmnopqrstuvwxyz)?");
     userIncludeInt = window.confirm("Would you like to include integers (0123456789)?");
     userIncludeSpec = window.confirm("Would you like to include special characters (~!@#$%^&*.?-_+={}()[]|\/<>)?");
-  } 
-
+    if (!userIncludeUpper && !userIncludeLower && !userIncludeInt && !userIncludeSpec) {
+      window.alert("You must select at least one category.");
+    }
+  }
   while (!userIncludeUpper && !userIncludeLower && !userIncludeInt && !userIncludeSpec);
-    
+
   if (userIncludeUpper) {
     possible += upperCase;
   }
@@ -50,14 +51,14 @@ function writePassword() {
   if (userIncludeSpec) {
     possible += specChar;
   }
-  
+
   for (let i = 0; i < userPrompt; i++) {
     userPass += possible.charAt(Math.floor(Math.random() * possible.length));
-  }  
+  }
 
-//var password = generatePassword();
- 
-passwordText.value = userPass;
+  //var password = generatePassword();
+
+  passwordText.value = userPass;
 
 }
 
